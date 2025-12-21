4daebbdffa817055c6a5ff6a7e0482bf1333a7d6
sabbir <sabbir00921@gmail.com>
2025-12-22 01:45:00 +0600

    Initial Boiler Plate

--- src/routes/index.api.js
+++ src/routes/index.api.js
@@ -2,10 +2,5 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/auth", require("./api/auth.api"));
-router.use("/job", require("./api/job.api"));
-router.use("/application", require("./api/application.api"));
-router.use("/payment", require("./api/payment.api"));
-router.use("/invoice", require("./api/invoice.api"));
-router.use("/analytics", require("./api/analytics.api"));
 
 module.exports = router;
